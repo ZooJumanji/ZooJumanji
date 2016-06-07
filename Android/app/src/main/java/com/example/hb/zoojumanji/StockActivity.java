8b47c3594903a51363d4b9e1954dd305afc5bebb
jonas Bardin <isher36@gmail.com>
2016-06-07 15:41:21 +0200

    ajout du formulaire d'ajout de stock

--- Android/app/src/main/java/com/example/hb/zoojumanji/StockActivity.java
+++ Android/app/src/main/java/com/example/hb/zoojumanji/StockActivity.java
@@ -1,6 +1,7 @@
 package com.example.hb.zoojumanji;
 
 import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -44,6 +45,18 @@ public class StockActivity extends AppCompatActivity {
                 TextView id_text = (TextView) view.findViewById(R.id.stock_id);
                 intent.putExtra("id", Integer.valueOf(String.valueOf(id_text.getText())));
 
+                startActivity(intent);
+            }
+        });
+
+        // Get clicked floatingButton to add a new animal
+        FloatingActionButton button = (FloatingActionButton) findViewById(R.id.add_fab);
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                // Generate new Activity
+                Intent intent = new Intent(StockActivity.this, StockCreationActivity.class);
                 startActivity(intent);
             }
         });
