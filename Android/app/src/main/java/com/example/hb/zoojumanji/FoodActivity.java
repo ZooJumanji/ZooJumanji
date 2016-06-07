f176b363774c29e62e34b37d384d8fa3fc1b8276
jonas Bardin <isher36@gmail.com>
2016-06-07 13:22:32 +0200

    modif et reorganisation du code apres merge

--- Android/app/src/main/java/com/example/hb/zoojumanji/FoodActivity.java
+++ Android/app/src/main/java/com/example/hb/zoojumanji/FoodActivity.java
@@ -1,7 +1,19 @@
 package com.example.hb.zoojumanji;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.example.hb.zoojumanji.adapter.StockAdapter;
+import com.example.hb.zoojumanji.dataManager.DataManager;
+import com.example.hb.zoojumanji.object.Stock;
+
+import java.util.List;
 
 public class FoodActivity extends AppCompatActivity {
 
@@ -9,5 +21,17 @@ public class FoodActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_food);
+
+        // Get list of stock
+        List<Stock> list = DataManager.getStocks();
+
+        // Generate specific adaptper
+        ArrayAdapter<Stock> adapter = new StockAdapter(this,
+                R.layout.list_stock_item, R.id.stock_name, list);
+
+        // Display list
+        ListView listView = (ListView) findViewById(R.id.stocks_list);
+        listView.setAdapter(adapter);
+
     }
 }
--- Android/app/src/main/java/com/example/hb/zoojumanji/dataManager/DataManager.java
+++ Android/app/src/main/java/com/example/hb/zoojumanji/dataManager/DataManager.java
@@ -1,8 +1,8 @@
 package com.example.hb.zoojumanji.dataManager;
 
-import com.example.hb.zoojumanji.R;
 import com.example.hb.zoojumanji.object.Animal;
 import com.example.hb.zoojumanji.object.Enclosure;
+import com.example.hb.zoojumanji.object.Stock;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,19 +12,15 @@ import java.util.List;
  */
 public class DataManager {
 
+    /******************* ANIMAL *********************************************/
     // Static animals list
-    public static final Animal SIMBA   = new Animal(0, "Simba", 8, Animal.SEX_MALE, Animal.SPECIES_LION, Animal.TYPE_CARNIVOROUS);
-    public static final Animal TIMON   = new Animal(1, "Timon", 12, Animal.SEX_MALE, Animal.SPECIES_SURICAT, Animal.TYPE_INSECTIVOROUS);
-    public static final Animal PUMBA   = new Animal(2, "Pumba", 15, Animal.SEX_MALE, Animal.SPECIES_WARTHOG, Animal.TYPE_OMNIVOROUS);
-    public static final Animal NALA    = new Animal(3, "Nala", 8, Animal.SEX_FEMALE, Animal.SPECIES_LION, Animal.TYPE_CARNIVOROUS);
+    public static final Animal SIMBA = new Animal(0, "Simba", 8, Animal.SEX_MALE, Animal.SPECIES_LION, Animal.TYPE_CARNIVOROUS);
+    public static final Animal TIMON = new Animal(1, "Timon", 12, Animal.SEX_MALE, Animal.SPECIES_SURICAT, Animal.TYPE_INSECTIVOROUS);
+    public static final Animal PUMBA = new Animal(2, "Pumba", 15, Animal.SEX_MALE, Animal.SPECIES_WARTHOG, Animal.TYPE_OMNIVOROUS);
+    public static final Animal NALA = new Animal(3, "Nala", 8, Animal.SEX_FEMALE, Animal.SPECIES_LION, Animal.TYPE_CARNIVOROUS);
     public static final Animal RAFIKKI = new Animal(4, "Rafikki", 82, Animal.SEX_MALE, Animal.SPECIES_MONKEY, Animal.TYPE_OMNIVOROUS);
 
-    public static final Enclosure LION_FOSS   = new Enclosure(0, "lion foss", 2, Enclosure.ENCLOSURE_TYPE_PADDOCK);
-    public static final Enclosure MONKEY_CAGE   = new Enclosure(1, "Rafikki cage", 12, Enclosure.ENCLOSURE_TYPE_CAGE);
-    public static final Enclosure TIMON_POOL   = new Enclosure(2, "Timon pool", 4, Enclosure.ENCLOSURE_TYPE_POOL);
-
     protected static List<Animal> animalsList = new ArrayList<>();
-    protected static List<Enclosure> enclosuresList = new ArrayList<>();
 
     public static List<Animal> getAnimals() {
 
@@ -44,13 +40,50 @@ public class DataManager {
     public static Animal getAnimal(int id) {
         List<Animal> list = getAnimals();
         for (Animal animal : list) {
-            if(animal.getId() == id) {
+            if (animal.getId() == id) {
                 return animal;
             }
         }
 
         throw new IllegalArgumentException("Unknown animal");
     }
+
+    /********************** STOCK *******************************************/
+    // static Stock list
+    public static final Stock HAY = new Stock(0, "Hay", Stock.TYPE_FOOD, 8, 10);
+    public static final Stock PEANUT = new Stock(0, "Peanut", Stock.TYPE_FOOD, 75, 100);
+
+    protected static List<Stock> stockList = new ArrayList<>();
+
+    public static List<Stock> getStocks() {
+        // Initialize list if is empty
+        if (stockList.isEmpty()) {
+            stockList.add(HAY);
+            stockList.add(PEANUT);
+        }
+        return stockList;
+    }
+
+    // Get Stock from id
+    public static Stock getStock(int id) {
+        List<Stock> list = getStocks();
+        for (Stock stock : list) {
+            if (stock.getId() == id) {
+                return stock;
+            }
+        }
+
+        throw new IllegalArgumentException("Unknown animal");
+    }
+
+
+    /******************** Enclosure **************************************/
+    // Static Enclosure List
+    public static final Enclosure LION_FOSS = new Enclosure(0, "lion foss", 2, Enclosure.ENCLOSURE_TYPE_PADDOCK);
+    public static final Enclosure MONKEY_CAGE = new Enclosure(1, "Rafikki cage", 12, Enclosure.ENCLOSURE_TYPE_CAGE);
+    public static final Enclosure TIMON_POOL = new Enclosure(2, "Timon pool", 4, Enclosure.ENCLOSURE_TYPE_POOL);
+
+    protected static List<Enclosure> enclosuresList = new ArrayList<>();
 
     public static List<Enclosure> getEnclosures() {
 
@@ -74,7 +107,7 @@ public class DataManager {
     public static Enclosure getEnclosure(int id) {
         List<Enclosure> list = getEnclosures();
         for (Enclosure enclosure : list) {
-            if(enclosure.getId() == id) {
+            if (enclosure.getId() == id) {
                 return enclosure;
             }
         }
