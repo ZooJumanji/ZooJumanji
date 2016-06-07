47c16c084d7f0bba0c8ba2ba8906cca4223a709b
motofondue <motofondue@no-log.org>
2016-06-07 14:41:22 +0200

    Add Ticket class

--- Android/app/src/main/java/com/example/hb/zoojumanji/object/Ticket.java
+++ Android/app/src/main/java/com/example/hb/zoojumanji/object/Ticket.java
@@ -1,6 +1,9 @@
 package com.example.hb.zoojumanji.object;
 
+import com.example.hb.zoojumanji.R;
+
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -8,10 +11,22 @@ import java.util.List;
  */
 public class Ticket {
 
+    public static final int TICKET_PRIVILEGE_FEED_TIMON = R.string.ticket_privilege_feed_timon;
+    public static final int TICKET_PRIVILEGE_CARE_ENTRANCE = R.string.ticket_privilege_care_entrance;
+    public static final int TICKET_PRIVILEGE_ENCLOSURE_ENTRANCE = R.string.ticket_privilege_enclosure_entrance;
+    public static final int TICKET_PRIVILEGE_NURSERY_ENTRANCE = R.string.ticket_privilege_nursery_entrance;
+
+    public static final int TICKET_TYPE_NORMAL = R.string.ticket_privilege_feed_timon;
+    public static final int TICKET_TYPE_VIP = R.string.ticket_privilege_care_entrance;
+    public static final int TICKET_TYPE_STUDENT = R.string.ticket_privilege_enclosure_entrance;
+    public static final int TICKET_TYPE_CHILD = R.string.ticket_privilege_nursery_entrance;
+    public static final int TICKET_TYPE_GROUP = R.string.ticket_privilege_nursery_entrance;
+
     protected int id;
     protected double price;
-    protected int quantity;
-    protected List<String> privileges = new ArrayList<>();
+    protected int type;
+    protected Date saleDate;
+    protected List<Integer> privileges = new ArrayList<>();
 
     public int getId() {
         return id;
@@ -31,27 +46,36 @@ public class Ticket {
         return this;
     }
 
-    public int getQuantity() {
-        return quantity;
+    public int getType() {
+        return type;
     }
 
-    public Ticket setQuantity(int quantity) {
-        this.quantity = quantity;
+    public Ticket setType(int type) {
+        this.type = type;
         return this;
     }
 
-    public List<String> getPrivileges() {
+    public Date getSaleDate() {
+        return saleDate;
+    }
+
+    public Ticket setDate(Date date) {
+        this.saleDate = date;
+        return this;
+    }
+
+    public List<Integer> getPrivileges() {
         return privileges;
     }
 
-    public Ticket addPrivilege(String privilege) {
+    public Ticket addPrivilege(int privilege) {
         if (!privileges.contains(privilege)) {
             privileges.add(privilege);
         }
         return this;
     }
 
-    public Ticket removePrivilege(String privilege) {
+    public Ticket removePrivilege(int privilege) {
         if (privileges.contains(privilege)) {
             privileges.remove(privilege);
         }
