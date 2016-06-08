fd1e8782f19564c1adf60e06787135413c14d35c
motofondue <motofondue@no-log.org>
2016-06-08 09:02:11 +0200

    Refactor TicketManager

--- Android/app/src/main/java/com/example/hb/zoojumanji/dataManager/TicketManager.java
+++ Android/app/src/main/java/com/example/hb/zoojumanji/dataManager/TicketManager.java
@@ -12,14 +12,20 @@ import java.util.List;
  */
 public class TicketManager {
     // Static tickets list
-    public static final Ticket TICKET_1 = new Ticket(0, Ticket.TICKET_TYPE_NORMAL, 8.50, 12, new Date(1478375387));
-    public static final Ticket TICKET_2 = new Ticket(1, Ticket.TICKET_TYPE_NORMAL, 8.50, 120, new Date(1478375753));
-    public static final Ticket TICKET_3 = new Ticket(2, Ticket.TICKET_TYPE_VIP, 18.00, 22, new Date(1478375852));
-    public static final Ticket TICKET_4 = new Ticket(3, Ticket.TICKET_TYPE_STUDENT, 6.50, 34, new Date(1478375987));
-    public static final Ticket TICKET_5 = new Ticket(4, Ticket.TICKET_TYPE_CHILD, 4.50, 16, new Date(1478375321));
-    public static final Ticket TICKET_6 = new Ticket(5, Ticket.TICKET_TYPE_GROUP, 7.00, 2, new Date(1478375741));
-    public static final Ticket TICKET_7 = new Ticket(6, Ticket.TICKET_TYPE_CHILD, 4.50, 3, new Date(1478375852));
-    public static final Ticket TICKET_8 = new Ticket(7, Ticket.TICKET_TYPE_CHILD, 4.50, 5, new Date(1478375654));
+
+    public static final int DAY_IN_MILLISECONDS = 1000 * 60 * 60 * 24;
+    public static final Date TODAY = new Date(System.currentTimeMillis());
+    public static final Date YESTERDAY = new Date(System.currentTimeMillis() - DAY_IN_MILLISECONDS);
+    public static final Date BEFORE_YESTERDAY = new Date(System.currentTimeMillis() - 2 * DAY_IN_MILLISECONDS);
+
+    public static final Ticket TICKET_NORMAL_TODAY = new Ticket(0, Ticket.TICKET_TYPE_NORMAL, 8.50, 12, TODAY);
+    public static final Ticket TICKET_NORMAL_YESTERDAY = new Ticket(1, Ticket.TICKET_TYPE_NORMAL, 8.50, 120, YESTERDAY);
+    public static final Ticket TICKET_VIP_TODAY = new Ticket(2, Ticket.TICKET_TYPE_VIP, 18.00, 22, TODAY);
+    public static final Ticket TICKET_STUDENT_TODAY = new Ticket(3, Ticket.TICKET_TYPE_STUDENT, 6.50, 34, TODAY);
+    public static final Ticket TICKET_GROUP_YESTERDAY = new Ticket(4, Ticket.TICKET_TYPE_GROUP, 7.00, 2, YESTERDAY);
+    public static final Ticket TICKET_CHILD_TODAY = new Ticket(5, Ticket.TICKET_TYPE_CHILD, 4.50, 16, TODAY);
+    public static final Ticket TICKET_CHILD_YESTERDAY = new Ticket(6, Ticket.TICKET_TYPE_CHILD, 4.50, 3, YESTERDAY);
+    public static final Ticket TICKET_CHILD_BEFORE_YESTERDAY = new Ticket(7, Ticket.TICKET_TYPE_CHILD, 4.50, 5, BEFORE_YESTERDAY);
 
     protected static List<Ticket> ticketsList = new ArrayList<>();
 
@@ -27,14 +33,14 @@ public class TicketManager {
 
         // Initialize list if is empty
         if (ticketsList.isEmpty()) {
-            ticketsList.add(TICKET_1);
-            ticketsList.add(TICKET_2);
-            ticketsList.add(TICKET_3);
-            ticketsList.add(TICKET_4);
-            ticketsList.add(TICKET_5);
-            ticketsList.add(TICKET_6);
-            ticketsList.add(TICKET_7);
-            ticketsList.add(TICKET_8);
+            ticketsList.add(TICKET_NORMAL_TODAY);
+            ticketsList.add(TICKET_NORMAL_YESTERDAY);
+            ticketsList.add(TICKET_VIP_TODAY);
+            ticketsList.add(TICKET_STUDENT_TODAY);
+            ticketsList.add(TICKET_GROUP_YESTERDAY);
+            ticketsList.add(TICKET_CHILD_TODAY);
+            ticketsList.add(TICKET_CHILD_YESTERDAY);
+            ticketsList.add(TICKET_CHILD_BEFORE_YESTERDAY);
         }
 
         return ticketsList;
