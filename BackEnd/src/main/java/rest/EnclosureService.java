13e838066bc8f4c23a82fd0bbcfe97b4273bc594
jonas Bardin <isher36@gmail.com>
2016-06-17 15:35:54 +0200

    nettoyage. creation de la version animal... erreur de ma part

--- BackEnd/src/main/java/rest/EnclosureService.java
+++ BackEnd/src/main/java/rest/EnclosureService.java
@@ -1,7 +1,5 @@
 package rest;
 
-import animal.Animal;
-import animal.repository.AnimalRepository;
 import enclosures.Enclosure;
 import enclosures.EnclosureType;
 
@@ -14,32 +12,23 @@ import java.util.List;
 @Path("/")
 public class EnclosureService {
 
-	public static final Enclosure LION_FOSS = new Enclosure("lion foss", 2, EnclosureType.PADDOCK);
-	public static final Enclosure MONKEY_CAGE = new Enclosure("Rafikki cage", 12, EnclosureType.CAGE);
-	public static final Enclosure TIMON_POOL = new Enclosure("Timon pool", 4, EnclosureType.POOL);
+    public static final Enclosure LION_FOSS = new Enclosure("lion foss", 2, EnclosureType.PADDOCK);
+    public static final Enclosure MONKEY_CAGE = new Enclosure("Rafikki cage", 12, EnclosureType.CAGE);
+    public static final Enclosure TIMON_POOL = new Enclosure("Timon pool", 4, EnclosureType.POOL);
 
-	private static List<Enclosure> list = new ArrayList<>();
+    private static List<Enclosure> list = new ArrayList<>();
 
-	@GET
-	@Path("enclosures")
-	@Produces({ "application/json" })
-	public List<Enclosure> getEnclosuresList() {
-		
-		if (list.isEmpty()) {
-			list.add(LION_FOSS);
-			list.add(MONKEY_CAGE);
-			list.add(TIMON_POOL);
-		}
-		
-		return list;
-	}
+    @GET
+    @Path("enclosures")
+    @Produces({"application/json"})
+    public List<Enclosure> getEnclosuresList() {
 
-	@GET
-	@Path("animaux")
-	@Produces({ "application/json" })
-	public List<Animal> getAnimalList() {
-		AnimalRepository repo = new AnimalRepository();
-		List<Animal> maList = repo.GetList();
-		return maList;
-	}
+        if (list.isEmpty()) {
+            list.add(LION_FOSS);
+            list.add(MONKEY_CAGE);
+            list.add(TIMON_POOL);
+        }
+
+        return list;
+    }
 }
