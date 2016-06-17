b722ff7200dbf07cc927da63433e100b3467d8b2
motofondue <motofondue@no-log.org>
2016-06-17 16:02:42 +0200

    Refactor AnimalRepository

--- BackEnd/src/main/java/animal/repository/AnimalRepository.java
+++ BackEnd/src/main/java/animal/repository/AnimalRepository.java
@@ -13,19 +13,20 @@ import java.util.List;
  */
 public class AnimalRepository {
 
+    // Static animals list
+    public static final Animal SIMBA = new Animal("Simba", 8, AnimalSex.MALE, AnimalSpecies.LION, AnimalType.CARNIVOROUS);
+    public static final Animal TIMON = new Animal("Timon", 12, AnimalSex.MALE, AnimalSpecies.SURICATE, AnimalType.INSECTIVOROUS);
+    public static final Animal PUMBA = new Animal("Pumba", 15, AnimalSex.MALE, AnimalSpecies.WARTHOG, AnimalType.OMNIVOROUS);
+    public static final Animal NALA = new Animal("Nala", 8, AnimalSex.FEMALE, AnimalSpecies.LION, AnimalType.CARNIVOROUS);
+    public static final Animal RAFIKKI = new Animal("Rafikki", 82, AnimalSex.MALE, AnimalSpecies.MONKEY, AnimalType.OMNIVOROUS);
+
+    List<Animal> animalsList = new ArrayList<>();
+    
     /**
      * Retourne la liste des animaux
      * @return List<Animal>
      */
-    public List<Animal> GetList() {
-        // Static animals list
-        Animal SIMBA = new Animal("Simba", 8, AnimalSex.MALE, AnimalSpecies.LION, AnimalType.CARNIVOROUS);
-        Animal TIMON = new Animal("Timon", 12, AnimalSex.MALE, AnimalSpecies.SURICATE, AnimalType.INSECTIVOROUS);
-        Animal PUMBA = new Animal("Pumba", 15, AnimalSex.MALE, AnimalSpecies.WARTHOG, AnimalType.OMNIVOROUS);
-        Animal NALA = new Animal("Nala", 8, AnimalSex.FEMALE, AnimalSpecies.LION, AnimalType.CARNIVOROUS);
-        Animal RAFIKKI = new Animal("Rafikki", 82, AnimalSex.MALE, AnimalSpecies.MONKEY, AnimalType.OMNIVOROUS);
-
-        List<Animal> animalsList = new ArrayList<>();
+    public List<Animal> getList() {
         if (animalsList.isEmpty()) {
             animalsList.add(SIMBA);
             animalsList.add(TIMON);
